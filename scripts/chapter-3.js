b204699bee05b759c0d6793a9202a96b0d3ec132
Priyam Raj <hi@priyamraj.com>
2021-12-01 02:19:26 +0530

    Fixed Countdown and Tweaked Backend
    Fixed Chapter 2 Season 19 > Chapter 3 Season 1

--- scripts/chapter-3.js
+++ scripts/chapter-3.js
@@ -6,7 +6,7 @@
 		var nextyear = month + '/' + day + '/' + year + ' 07:07:07';
 
 		$('#example').countdown({
-			date: '12/05/21 20:05:00', // TODO Date format: 07/27/2017 17:00:00
+			date: '12/04/21 21:05:00', // TODO Date format: 07/27/2017 17:00:00
 			// offset: +3, // TODO Your Timezone Offset
 			day: 'Day',
 			days: 'Days'
