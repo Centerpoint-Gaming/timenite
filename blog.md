88ed693a6a33508866ed5fc2bf9d8e1106a106ae
Priyam Raj <hello@priyamraj.com>
2022-07-05 20:38:16 +0530

    Added Navbar & Seperators

--- blog.md
+++ blog.md
@@ -5,11 +5,12 @@ layout: page
 
 
 <div class="container pt-1">
-  <div class="row">
-    <div class="text-center">
-      <ul class="paragraph" style="list-style-position:inside">
-        {% for post in site.posts%}
-          <li  class= "timenite-black">
-          <span>{{ post.date | date: "%d %B %Y"}}</span> - <a href="{{ post.url }}">{{ post.title }}</a>
-            {% endfor %}
-          </li>
+  <ul class="paragraph">
+    {% for post in site.posts%}
+    <li class="timenite-black is-size-5">
+      {{ post.date | date: "%d %B %Y"}} - <a href="{{ post.url }}">{{ post.title }}</a>
+    </li>
+    {% endfor %}
+  </ul>
+</div>
+
--- scripts/main.js
+++ scripts/main.js
@@ -108,4 +108,29 @@
 
 
 
+//  For Navbar
+document.addEventListener('DOMContentLoaded', function () {
 
+	var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+	if ($navbarBurgers.length > 0) {
+  	  $navbarBurgers.forEach(function ($el) {
+		$el.addEventListener('click', function () {
+  
+		  var target = $el.dataset.target;
+		  var $target = document.getElementById(target);
+  
+		  $el.classList.toggle('is-active');
+		  $target.classList.toggle('is-active');
+  
+		});
+	  });
+	}
+  
+  });
+  
+  document.querySelectorAll('.navbar-link').forEach(function(navbarLink){
+	navbarLink.addEventListener('click', function(){
+	  navbarLink.nextElementSibling.classList.toggle('is-hidden-mobile');
+	})
+  });
