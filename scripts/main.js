2edc1e09fc1b164978e2cd32788584e44df8301d
Priyam Raj <hello@priyamraj.com>
2022-07-19 23:19:44 +0530

    Scripts => main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,136 +1,186 @@
 (function ($) {
-	$.fn.countdown = function (options, callback) {
-		var settings = $.extend({
-			date: null,
-			offset: null,
-			day: 'Day',
-			days: 'Days',
-			hour: 'Hour',
-			hours: 'Hours',
-			minute: 'Minute',
-			minutes: 'Minutes',
-			second: 'Second',
-			seconds: 'Seconds'
-		}, options);
+  $.fn.countdown = function (options, callback) {
+    var settings = $.extend(
+      {
+        date: null,
+        offset: null,
+        day: "Day",
+        days: "Days",
+        hour: "Hour",
+        hours: "Hours",
+        minute: "Minute",
+        minutes: "Minutes",
+        second: "Second",
+        seconds: "Seconds",
+      },
+      options
+    );
 
-		// Throw error if date is not set
-		if (!settings.date) {
-			$.error('Date is not defined.');
-		}
+    // Throw error if date is not set
+    if (!settings.date) {
+      $.error("Date is not defined.");
+    }
 
-		// Throw error if date is set incorectly
-		if (!Date.parse(settings.date)) {
-			$.error('Incorrect date format, it should look like this, 12/24/2012 12:00:00.');
-		}
+    // Throw error if date is set incorectly
+    if (!Date.parse(settings.date)) {
+      $.error(
+        "Incorrect date format, it should look like this, 12/24/2012 12:00:00."
+      );
+    }
 
-		// Save container
-		var container = this;
+    // Save container
+    var container = this;
 
-		/**
-		 * Change client's local date to match offset timezone
-		 * @return {Object} Fixed Date object.
-		 */
-		var currentDate = function () {
-			// get client's current date
-			var date = new Date();
+    /**
+     * Change client's local date to match offset timezone
+     * @return {Object} Fixed Date object.
+     */
+    var currentDate = function () {
+      // get client's current date
+      var date = new Date();
 
-			// turn date to utc
-			var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+      // turn date to utc
+      var utc = date.getTime() + date.getTimezoneOffset() * 60000;
 
-			// set new Date object
-			var new_date = new Date(utc + (3600000*settings.offset));
+      // set new Date object
+      var new_date = new Date(utc + 3600000 * settings.offset);
 
-			return new_date;
-		};
+      return new_date;
+    };
 
-		/**
-		 * Main countdown function that calculates everything
-		 */
-		function countdown () {
-			var target_date = new Date(settings.date), // set target date
-				current_date = currentDate(); // get fixed current date
+    /**
+     * Main countdown function that calculates everything
+     */
+    function countdown() {
+      var target_date = new Date(settings.date), // set target date
+        current_date = currentDate(); // get fixed current date
 
-			// difference of dates
-			var difference = target_date - current_date;
+      // difference of dates
+      var difference = target_date - current_date;
 
-			// if difference is negative than it's pass the target date
-			if (difference < 0) {
-				// stop timer
-				clearInterval(interval);
+      // if difference is negative than it's pass the target date
+      if (difference < 0) {
+        // stop timer
+        clearInterval(interval);
 
-				if (callback && typeof callback === 'function') callback();
+        if (callback && typeof callback === "function") callback();
 
-				return;
-			}
+        return;
+      }
 
-			// basic math variables
-			var _second = 1000,
-				_minute = _second * 60,
-				_hour = _minute * 60,
-				_day = _hour * 24;
+      // basic math variables
+      var _second = 1000,
+        _minute = _second * 60,
+        _hour = _minute * 60,
+        _day = _hour * 24;
 
-			// calculate dates
-			var days = Math.floor(difference / _day),
-				hours = Math.floor((difference % _day) / _hour),
-				minutes = Math.floor((difference % _hour) / _minute),
-				seconds = Math.floor((difference % _minute) / _second);
+      // calculate dates
+      var days = Math.floor(difference / _day),
+        hours = Math.floor((difference % _day) / _hour),
+        minutes = Math.floor((difference % _hour) / _minute),
+        seconds = Math.floor((difference % _minute) / _second);
 
-			// based on the date change the refrence wording
-			var text_days = (days === 1) ? settings.day : settings.days,
-				text_hours = (hours === 1) ? settings.hour : settings.hours,
-				text_minutes = (minutes === 1) ? settings.minute : settings.minutes,
-				text_seconds = (seconds === 1) ? settings.second : settings.seconds;
+      // based on the date change the refrence wording
+      var text_days = days === 1 ? settings.day : settings.days,
+        text_hours = hours === 1 ? settings.hour : settings.hours,
+        text_minutes = minutes === 1 ? settings.minute : settings.minutes,
+        text_seconds = seconds === 1 ? settings.second : settings.seconds;
 
-				// fix dates so that it will show two digets
-				days = (String(days).length >= 2) ? days : '0' + days;
-				hours = (String(hours).length >= 2) ? hours : '0' + hours;
-				minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
-				seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
+      // fix dates so that it will show two digets
+      days = String(days).length >= 2 ? days : "0" + days;
+      hours = String(hours).length >= 2 ? hours : "0" + hours;
+      minutes = String(minutes).length >= 2 ? minutes : "0" + minutes;
+      seconds = String(seconds).length >= 2 ? seconds : "0" + seconds;
 
-			// set to DOM
-			container.find('.days-left').text(days);
-			container.find('.hours').text(hours);
-			container.find('.minutes').text(minutes);
-			container.find('.seconds').text(seconds);
+      // set to DOM
+      container.find(".days-left").text(days);
+      container.find(".hours").text(hours);
+      container.find(".minutes").text(minutes);
+      container.find(".seconds").text(seconds);
 
-			container.find('.days_text').text(text_days);
-			container.find('.hours_text').text(text_hours);
-			container.find('.minutes_text').text(text_minutes);
-			container.find('.seconds_text').text(text_seconds);
-		}
+      container.find(".days_text").text(text_days);
+      container.find(".hours_text").text(text_hours);
+      container.find(".minutes_text").text(text_minutes);
+      container.find(".seconds_text").text(text_seconds);
+    }
 
-		// start
-		var interval = setInterval(countdown, 1000);
-	};
+    // start
+    var interval = setInterval(countdown, 1000);
+  };
 })(jQuery);
 
 
 
 
 
-//  For Navbar
-document.addEventListener('DOMContentLoaded', function () {
 
-	var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-	if ($navbarBurgers.length > 0) {
-  	  $navbarBurgers.forEach(function ($el) {
-		$el.addEventListener('click', function () {
-  
-		  var target = $el.dataset.target;
-		  var $target = document.getElementById(target);
-  
-		  $el.classList.toggle('is-active');
-		  $target.classList.toggle('is-active');
-  
-		});
-	  });
-	}
-  
+// For Season Countdown
+var now = new Date();
+var day = now.getDate();
+var month = now.getMonth() + 1;
+var year = now.getFullYear() + 1;
+
+$("#full-countdown").countdown(
+  {
+    date: "09/17/22 04:00:00",
+    // ^ Change this to tweak the upcoming Season's time in UTC 00:00
+    // Date format: 07/27/2017 17:00:00
+
+    // offset: +3,
+    // ^ Uncomment for additional timezone offset
+    day: "Day",
+    days: "Days",
+  },
+  function () {
+    alert("Well, time to wait!");
+  }
+);
+
+// For Daily Item Shop
+var today = new Date();
+var tomorrow = new Date(today);
+tomorrow.setDate(today.getDate() + 1);
+var day = tomorrow.getDate();
+var month = tomorrow.getMonth() + 1;
+var year = tomorrow.getFullYear();
+
+var nextday = month + "/" + day + "/" + year + " 00:00:00";
+
+$("#clock").countdown(
+  {
+    date: nextday,
+    day: "Day",
+    days: "Days",
+  },
+
+  function () {
+    day++;
+    location.reload();
+  }
+);
+
+//  For Navbar
+document.addEventListener("DOMContentLoaded", function () {
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener("click", function () {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
+
+document.querySelectorAll(".navbar-link").forEach(function (navbarLink) {
+  navbarLink.addEventListener("click", function () {
+    navbarLink.nextElementSibling.classList.toggle("is-hidden-mobile");
   });
-  
-  document.querySelectorAll('.navbar-link').forEach(function(navbarLink){
-	navbarLink.addEventListener('click', function(){
-	  navbarLink.nextElementSibling.classList.toggle('is-hidden-mobile');
-	})
-  });
+});
