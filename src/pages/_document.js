1e9755d3680d31f2d75f30d1f62c599c28b84f74
Priyam Raj <hello@priyamraj.com>
2024-04-15 20:49:34 +0530

    - Added favicon - Adds CPG ads - Added meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='0.9em' font-size='90' font-weight='bold'%3E⏰%3C/text%3E%3C/svg%3E" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home () {
   const [progress, setProgress] = useState(6)
@@ -7,6 +8,7 @@ export default function Home () {
   const [seasonNumber, setSeasonNumber] = useState(0)
   const [startDate, setStartDate] = useState(null)
   const [countdownDate, setCountdownDate] = useState(null)
+  const [crosshairIndex, setCrosshairIndex] = useState(1)
 
   useEffect(() => {
     const fetchCountdown = async () => {
@@ -15,13 +17,12 @@ export default function Home () {
       setStartDate(data.startDate)
       setCountdownDate(data.countdownDate)
       setSeasonNumber(data.seasonNumber)
-      console.log(data)
       return data
     }
 
-    fetchCountdown() // Fetch immediately on component mount
+    fetchCountdown()
 
-    const fetchInterval = setInterval(fetchCountdown, 5000) // Then every 5 seconds
+    const fetchInterval = setInterval(fetchCountdown, 5000)
 
     return () => clearInterval(fetchInterval)
   }, [])
@@ -60,17 +61,51 @@ export default function Home () {
       )
     }
 
-    const realTimeInterval = setInterval(updateRealTimeCountdown, 1) // Update every millisecond
+    const realTimeInterval = setInterval(updateRealTimeCountdown, 1)
 
     return () => clearInterval(realTimeInterval)
-  }, [countdownDate, startDate]) // Depend on countdownDate and startDate to restart the interval when they change
+  }, [countdownDate, startDate])
+
+  useEffect(() => {
+    const crosshairInterval = setInterval(() => {
+      setCrosshairIndex(prevIndex => (prevIndex === 12 ? 1 : prevIndex + 1))
+    }, 1000)
+
+    return () => clearInterval(crosshairInterval)
+  }, [])
+
+  const renderAdImageWithCrosshair = (adSrc, width, height, className = '') => (
+    <div className={`relative inline-flex justify-center ${className}`}>
+      <Image src={adSrc} alt='Ad' width={width} height={height} />
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+        <Image
+          src={`/Timenite Ads/Crosshairs to Overlay/CrosshairV2/${crosshairIndex}.png`}
+          alt='Crosshair'
+          width={30}
+          height={30}
+        />
+      </div>
+    </div>
+  )
 
   return (
-    <div className='flex min-h-screen flex-col bg-timenitePrimary'>
+    <div className='flex min-h-screen flex-col bg-timenitePrimary items-center'>
       <Head>
         <title>Season 2 Countdown</title>
+        <meta
+          name='description'
+          content='Countdown to the next Season on Fortnite'
+        />
+        <meta
+          name='keywords'
+          content='Fortnite, Season 2, Countdown, Timenite'
+        />
+        <link
+          rel='icon'
+          href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='0.9em' font-size='90' font-weight='bold'%3E⏰%3C/text%3E%3C/svg%3E"
+        />
       </Head>
-      <nav className='flex justify-between items-center p-4 text-white'>
+      <nav className='w-full flex justify-between items-center p-4 text-white'>
         <span className='text-3xl'>Timenite</span>
         <div className='flex gap-4'>
           <a href='https://twitter.com/timenitedaily' className='text-white'>
@@ -81,7 +116,21 @@ export default function Home () {
           </a>
         </div>
       </nav>
-      <div className='flex-grow flex items-center justify-center'>
+      {renderAdImageWithCrosshair(
+        '/Timenite Ads/Mobile_X.png',
+        450,
+        300,
+        'md:hidden'
+      )}
+      <div className='flex-grow w-full flex flex-col md:flex-row items-center justify-center'>
+        <div className='flex-1 flex justify-start ml-2'>
+          {renderAdImageWithCrosshair(
+            '/Timenite Ads/PC_X.png',
+            200,
+            150,
+            'hidden md:inline-flex'
+          )}
+        </div>
         <div className='text-center'>
           <h1 className='text-6xl font-bold text-white'>Season 2 Countdown</h1>
           <div
@@ -115,7 +164,21 @@ export default function Home () {
 
           <p className='text-white mt-4 text-2xl'>{timeLeft}</p>
         </div>
+        <div className='flex-1 flex justify-end mr-2'>
+          {renderAdImageWithCrosshair(
+            '/Timenite Ads/PC_X.png',
+            200,
+            90,
+            'hidden md:inline-flex'
+          )}
+        </div>
       </div>
+      {renderAdImageWithCrosshair(
+        '/Timenite Ads/Mobile_V2.png',
+        450,
+        80,
+        'md:hidden'
+      )}
       <div className='flex items-center justify-center'>
         <p className='text-xl text-white mt-4'>
           This is the {seasonNumber}th season in Fortnite that started on{' '}
